1f227b12d8821386bd510975cbd3e69f4e7f70d1
ThaysonScript <thaysonguedes01@gmail.com>
2026-02-12 17:34:46 -0300

    tela de logout, apenas visual sem logica de negocio

---
+++ frontend/src/app/views/logout/logout.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-logout-view',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './logout.html'
+})
+export class LogoutView implements OnInit, OnDestroy {
+  contador = 15;
+  private timer: any;
+
+  ngOnInit(): void {
+    this.timer = setInterval(() => {
+      if (this.contador > 0) this.contador--;
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.timer) clearInterval(this.timer);
+  }
+}
